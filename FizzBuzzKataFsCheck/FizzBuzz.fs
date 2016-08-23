de3d80dcb1d565965635e5dfd3bb69c63ded319a
Magnus Härlin <magnus.harlin@gmail.com>
2016-08-23 11:30:24 +0200

    My first attempt at solving FizzBuzz with FsCheck

---
+++ FizzBuzzKataFsCheck/FizzBuzz.fs
@@ -0,0 +1,8 @@
+﻿module FizzBuzz
+let run (number : int) = 
+    match number % 3, number % 5 with
+    | 0, 0  -> "FizzBuzz"
+    | 0, _  -> "Fizz"
+    | _, 0 -> "Buzz"
+    | _ -> number.ToString()
+
---
+++ FizzBuzzKataFsCheck/FizzBuzzTests.fs
@@ -0,0 +1,36 @@
+﻿module FizzBuzzTests
+
+open FsCheck
+open FsCheck.Xunit
+
+[<Property>]
+let numberIsReturnedWhenNotDivisableBy3Or5 number = 
+    (number % 3 <> 0 && number % 5 <> 0) ==> lazy
+    FizzBuzz.run number = number.ToString()
+    
+
+[<Property>]
+let fizzIsReturnredWhenDivisableBy3() = 
+    let numberDivisibleBy3 = 
+        Arb.generate<int>
+        |> Gen.map ((*) 3) 
+        |> Gen.filter (fun n -> n % 5 <> 0) 
+        |> Arb.fromGen
+    Prop.forAll numberDivisibleBy3 (fun number -> FizzBuzz.run number = "Fizz")
+
+[<Property>]
+let buzzIsReturnredWhenDivisableBy5 (PositiveInt number) = 
+    let numberDivisibleBy5 = 
+        Arb.generate<int> 
+        |> Gen.map ((*) 5) 
+        |> Gen.filter (fun n -> n % 3 <> 0) 
+        |> Arb.fromGen
+    Prop.forAll numberDivisibleBy5 (fun number -> FizzBuzz.run number = "Buzz")
+
+[<Property>]
+let fizzBuzzIsReturnredWhenDivisableBy3and5 (PositiveInt number) = 
+    let numberDivisibleBy3and5 = 
+        Arb.generate<int> 
+        |> Gen.map ((*) (3 * 5))
+        |> Arb.fromGen
+    Prop.forAll numberDivisibleBy3and5 (fun number -> FizzBuzz.run number = "FizzBuzz")
